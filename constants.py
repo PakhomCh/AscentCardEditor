603248ad54332c89dddcf624d57a58af1b34b7f4
PakhomCh <pakhomch@gmail.com>
2023-09-13 22:06:42 +0300

    XML file structure added

---
+++ constants.py
@@ -0,0 +1,6 @@
+# Size of each Kontinuum Ascent set, Nontoken, Token, Total
+SETSIZE = {'default': (50, 5, 55), 'core': (250, 5, 255), 'base': (36, 0, 36), 'empty': (0, 0, 0), 'custom': (1, 0, 1)}
+
+# Colors of each card
+COLORS = ('white', 'black', 'red', 'blue', 'neutral')
+SHORTCOLORS = ('W', 'B', 'R', 'U', 'N')
